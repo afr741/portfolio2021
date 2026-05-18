67ee2939d3578bfb2e5bf9afd441da8b699a1443
Anushervon Rakhmat <your-atco-email@atco.com>
2026-05-18 00:09:47 -0400

    upgrade build configs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   turbopack: {
     root: __dirname,
-  },
-  images: {
-    unoptimized: true,
   },
   sassOptions: {
     includePaths: ['./src'],
   },
-  // Disable x-powered-by header for minor security improvement
   poweredByHeader: false,
 };
 
